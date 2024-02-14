11e1279ef027cf3e429cdf696c444852865aec90
Amalprneopraxis <amal.pr@neopraxis.in>
2024-02-14 14:35:43 +0530

    removed javascript

--- src/Megamenu.js
+++ src/Megamenu.js
@@ -1,36 +1,6 @@
-import React, { useState, useEffect } from 'react';
-import './components/header/Megamenu.css'
+import React from "react";
 
 const Megamenu = () => {
-    const [slideIndex, setSlideIndex] = useState(1);
-  
-    useEffect(() => {
-      const interval = setInterval(() => {
-        plusSlides(1);
-      }, 4000);
-      return () => clearInterval(interval);
-    }, [slideIndex]);
-  
-    const plusSlides = (n) => {
-      showSlides(slideIndex + n);
-    };
-  
-    const showSlides = (n) => {
-      let newIndex = n;
-      const slides = document.getElementsByClassName("mySlides");
-      if (n > slides.length) {
-        newIndex = 1;
-      }
-      if (n < 1) {
-        newIndex = slides.length;
-      }
-      setSlideIndex(newIndex);
-      for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-      }
-      slides[newIndex - 1].style.display = "block";
-    };
-  
     return (
       <div className="slideshow-container">
         <div className="mySlides">
