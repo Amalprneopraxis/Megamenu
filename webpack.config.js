bcde28ba814e3bb951a05212e894eb49d6a20fc2
Amalprneopraxis <amal.pr@neopraxis.in>
2024-02-14 14:25:26 +0530

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+// webpack.config.js
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/Megamenu.js', // Path to your component entry file
+  output: {
+    path: path.resolve(__dirname, 'dist'), // Output directory
+    filename: 'Megamenu.bundle.js', // Output filename
+    library: 'Megamenu', // Name of the library
+    libraryTarget: 'umd', // Output module format
+    umdNamedDefine: true, // Define the module's name for AMD
+    globalObject: 'this' // Define the global object used,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      
+    ],
+  },
+};
